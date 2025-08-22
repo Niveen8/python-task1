a0737143001688256ee73c482451931839382177
Niveen <niveenabutamam@gmail.com>
2025-08-22 19:10:50 +0300

    add read

--- taskone.py
+++ taskone.py
@@ -7,7 +7,8 @@ with open("file.txt","w") as f:
  f.write("Name: " + name + "\n")
  f.write("Age: " + age + "\n")
  f.write("city: " + city + "\n")
-
+with open("file.txt", "r") as f:
+print(f.read())
 
 
 

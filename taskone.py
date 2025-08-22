f6bbcaa27d8147fc4c6685306e48137336a34d04
Niveen <niveenabutamam@gmail.com>
2025-08-22 19:09:59 +0300

    add  write

--- taskone.py
+++ taskone.py
@@ -3,6 +3,12 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 city =input("Enter your city: ")
+with open("file.txt","w") as f:
+ f.write("Name: " + name + "\n")
+ f.write("Age: " + age + "\n")
+ f.write("city: " + city + "\n")
+
+
 
 
 
